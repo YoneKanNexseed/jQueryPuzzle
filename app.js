3a610b727e1a076face489ed088a81d6fd4312e5
obscure723 <obscure723@yone-lab.com>
2020-01-31 16:52:44 +0800

    no message

--- app.js
+++ app.js
@@ -7,8 +7,12 @@ let panelArray = [1,2,3,4,5,6,7,8,9,10,11,12];
 $(function () {
   // 画面が表示されたら、ここの処理が実行される
 
+  // shuffle関数を使って、配列をシャッフルする
+  // 結果を変数shuffledArrayに保存する
+  let shuffledArray = shuffle(panelArray);
+
   // 1 〜 12のパネルを表示する
-  panelArray.forEach(number => {
+  shuffledArray.forEach(number => {
 
     $('#puzzle-container').append(
       Panel({ num: number })
@@ -20,4 +24,40 @@ $(function () {
   $('#puzzle-container').sortable();
   $( "#puzzle-container" ).disableSelection();
 
- })
+})
+
+// 配列をシャッフルしてくれる関数
+function shuffle(array) {
+    let copyArray = [...array];
+    for (let i = copyArray.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let temp = copyArray[i];
+        copyArray[i] = copyArray[j];
+        copyArray[j] = temp;
+    }
+    return copyArray;
+}
+
+
+$('.btn-check').on('click', () => {
+
+  // 空の配列を用意
+  let userAnswer = [];
+
+  //1. 画面のliタグを全部取ってきて、配列に入れる
+  $('li').each(function () {
+    // console.log($(this).text())
+    userAnswer.push($(this).text());
+  })
+
+  //2. liタグの配列と、正解の配列を比較する
+  console.log(userAnswer.toString())
+  console.log(panelArray.toString())
+
+  if (userAnswer.toString() == panelArray.toString()) {
+    alert('正解')
+  } else {
+    alert('不正解');
+  }
+
+})
